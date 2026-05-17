20c813b5a911115d324627e8fd07186abacc8d3b
Dengtiel <d.akol@alustudent.com>
2026-05-17 21:47:03 +0200

    16-binary_tree_is_perfect: fix static declaration conflict

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Helper to measure height
+ * @tree: Pointer to the root node
+ * Return: Height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_h > right_h ? left_h : right_h);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node
+ *
+ * Return: 1 if perfect, 0 otherwise or if NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	if (left_h != right_h)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+}
