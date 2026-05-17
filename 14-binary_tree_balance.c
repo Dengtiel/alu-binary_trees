28f9e6e46dde0c051acc0b2d753bc64dcaec90a7
Dengtiel <d.akol@alustudent.com>
2026-05-17 22:05:49 +0200

    14-binary_tree_balance: fix height calculation for balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Helper to measure height
+ * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node
  * Return: Height of the tree
  */
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left_h = binary_tree_height(tree->left) + 1;
+	right_h = binary_tree_height(tree->right) + 1;
 	return (left_h > right_h ? left_h : right_h);
 }
 
